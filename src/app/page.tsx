6930a9c20fe68338dd2661cdbd04c4f8a91d7726
zViruzz <nahuelsinka@gmail.com>
2024-09-19 19:29:44 -0300

    update 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,102 @@
-import { css } from '../../styled-system/css';
+'use client'
+import { Input } from "@/components/ui/Input";
+import { css } from "../../styled-system/css";
+import { ChangeEvent, useEffect, useState } from "react";
+
+interface Product {
+  id: number
+  name: string
+  price: number
+}
 
 export default function Home() {
+  const [searchSuggestions, setSearchSearchSuggestions] = useState<Product[]>([])
+  const [products, setProducts] = useState<Product[] | null>(null)
+
+  useEffect(() => {
+    const suggestions = [
+      { id: 1, name: "Iphone", price: 10 },
+      { id: 2, name: "Mac", price: 20 },
+      { id: 3, name: "Asus Rog", price: 30 },
+      { id: 4, name: "Xiaomi mi 6", price: 40 },
+      { id: 5, name: "Samsung a32", price: 50 },
+    ];
+    setProducts(suggestions)
+  }, [])
+
+  const filterSearch = (value: string): Product[] => {    
+    if (products === null) {
+      return []
+    }
+    const filteredSuggestions = products.filter((product) =>
+      product.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    console.log('filteredSuggestions', filteredSuggestions)
+    return filteredSuggestions;
+  }
+
+  const onSuggestion = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+    
+    setSearchSearchSuggestions(filterSearch(event.target.value))
+  }
+
   return (
-    <div 
-      className={styles.container}
-    >
+    <div className={styles.container}>
       <div className={styles.containerForm}>
         <div>
-          <h1>
-            Hello 🐼!
-          </h1>
+          <h1>Hello 🐼!</h1>
         </div>
         <div>
-          <form action="">
-            <div>
-              <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" />
+          <form className={styles.form}>
+            <Input
+              onChange={onSuggestion}
+              placeholder="Search"
+            />
+            <div className={styles.autocompleteContainer}>
+              <ul>
+                {
+                  searchSuggestions.map(product => (
+                    <li key={product.id}>
+                      {product.name}
+                    </li>
+                  ))
+                }
+              </ul>
             </div>
           </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 const styles = {
   container: css({
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
+    display: "flex",
+    flexDirection: "column",
+    background: 'transparent',
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
   }),
   containerForm: css({
-    border: '1px solid',
-    borderColor: 'gray.500',
-    borderRadius: 'xl',
-    padding: '20px',
+    border: "1px solid",
+    borderColor: "gray.500",
+    borderRadius: "xl",
+    padding: "30px",
   }),
+  form: css({
+    position: 'relative'
+  }),
+  autocompleteContainer: css({
+    width: '100%',
+    marginTop: '5px',
+    padding: '.6rem',
+    position: 'absolute',
+    border: '1px solid #333333FF',
+    borderRadius: 'xl',
+    background: '#141414FF'
+  })
 }
---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,50 @@
+import { forwardRef, InputHTMLAttributes } from "react";
+import { css } from "../../../styled-system/css";
+import { SystemStyleObject } from "../../../styled-system/types";
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> { 
+  css?: SystemStyleObject;
+}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ css: cssProps, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={css(style,cssProps)}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+
+const style = css.raw({
+  display: "flex",
+  height: "2.5rem",
+  width: "100%",
+  maxWidth: "20rem",
+  borderRadius: ".45rem",
+  border: "1px solid",
+  borderColor: "#27272a",
+  backgroundColor: "#09090b",
+  padding: ".5rem .75rem",
+  fontSize: "14px",
+  transition: "ease-in-out .1s",
+  ringColor: "#000",
+  _focusVisible: {
+    ringOffset: "2px",
+    ringColor: "#d4d4d8",
+    ringWidth: "2px",
+    outline: "2px solid transparent",
+  },
+  _placeholder: {
+    color: "#a1a1aa",
+  },
+  _disabled: {
+    opacity: "50%",
+  }
+})
+
+export { Input }
